04b1a5d69aaf45661dbaa7ff7053a9814aa201f1
Jordan Hayden <keephayden@Jordans-MacBook-Pro.local>
2020-04-08 11:43:35 -0500

    program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace CrimeAnalyzer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length != 2) {
+                Console.WriteLine("CrimeAnalyzer <crime_csv_file_path> <report_file_path>");
+                Environment.Exit(1);
+            }
+
+            string csvDataFilePath = args[0];
+            string reportFilePath = args[1];
+
+            List<CrimeStats> crimeStatsList = null;
+            try
+            {
+                crimeStatsList = CrimeStatsLoader.Load(csvDataFilePath);
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+                Environment.Exit(2);
+            }
+
+            var report = CrimeStatsReport.GenerateText(crimeStatsList);
+
+            try
+            {
+                System.IO.File.WriteAllText(reportFilePath, report);
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+                Environment.Exit(3);
+            }
         }
     }
 }
